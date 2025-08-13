f0d06bd5d20f3f272fe6982cd26194a70894e5f3
kyonyhk <kuoloon@hotmail.com>
2025-08-13 13:34:26 +0800

    fix: update atoms index to remove deleted component exports
    - Remove Input and Button exports from atoms index
    - Keep only Logo component export
    - Fixes build error after component cleanup

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,8 +1,2 @@
-export { Input } from './Input';
-export type { InputProps } from './Input';
-
-export { Button } from './Button';
-export type { ButtonProps } from './Button';
-
 export { default as Logo } from './Logo';
 export type { LogoProps } from './Logo';
